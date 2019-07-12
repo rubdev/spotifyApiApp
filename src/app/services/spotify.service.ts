55896643183e532e0260e1a4529d8a74950f09be
Rubén Segura <rsegrom@gmail.com>
2019-07-12 17:52:41 +0200

    Fn devuelve objeto con ultimos lanzamientos spoti

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -14,13 +14,10 @@ export class SpotifyService {
     console.log('Spotify service -> Cargando nuevos lanzamientos en España');
     // Cabecera necesaria para que funcione el token
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCqe1UerqEzF4grFPrqMSIfdej9poO7xT2NcqTtWjlbhVkrTiqVk2RbtidwqcW5u3dGhY_4n8kmDVl9C1I'
+      'Authorization': 'Bearer BQC9Yd59VRZ3Ax51frqH-a6FDftM4HWe6Gnj2YVVpAxl5G12aqfLgBdRSWNUOUWmJzyOqkIkKZr8nG4QgIg'
     });
     // Para hacer la petición get con el token generado
-    this.clienteHttp.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-      .subscribe( datos => {
-        console.log(datos);
-      });
+    return this.clienteHttp.get('https://api.spotify.com/v1/browse/new-releases', { headers });
   }
 
 }
