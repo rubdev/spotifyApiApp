2eb077755a0443174060703c896f640735f5c846
Rubén Segura <rsegrom@gmail.com>
2019-07-12 19:27:25 +0200

    Add fn getQuery

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,30 +8,30 @@ import { map } from 'rxjs/operators';
 })
 export class SpotifyService {
 
-  token = 'BQCX1Yiu3WXZkn26mXwV_PSMN10zxJq7VoD9Jw2YbglaMqqxZJdukxdpkQGDOJcuihQi0NqR1rehgVRuseU';
+  token = 'BQAToq0IkDP5eM85r4VxiY4fb6BAuki7inakXr2_4AK6RKM_xzKH_2kyyykswUKfM4PuiII9yK0JwfmfxN4';
 
   constructor( private clienteHttp: HttpClient) {
     console.log('Servicio Spotify listo');
   }
 
-  getSpainNewReleases() {
-    console.log('Spotify service -> Cargando nuevos lanzamientos en España');
+  getQuery( query: string ) {
+    const url = `https://api.spotify.com/v1/${ query }`;
     // Cabecera necesaria para que funcione el token
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${ this.token }`
     });
+    return this.clienteHttp.get(url, { headers });
+  }
+
+  getSpainNewReleases() {
+    console.log('Spotify service -> Cargando nuevos lanzamientos en España');
     // Para hacer la petición get con el token generado
-    return this.clienteHttp.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-                  .pipe( map( data => data['albums'].items ) );
+    return this.getQuery('browse/new-releases').pipe( map( data => data['albums'].items ) );
   }
 
   getArtist( termino: string ) {
     console.log('Spotify service -> Cargando artistas encontrados');
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${ this.token }`
-    });
-    return this.clienteHttp.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=20`, { headers })
-                .pipe ( map( data =>  data['artists'].items ) );
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=20`).pipe ( map( data =>  data['artists'].items ) );
   }
 
 }
